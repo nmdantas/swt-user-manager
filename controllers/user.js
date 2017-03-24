8436f5bd0f54f007d6fab6505cab2add3c4c9e86
n.moraes.dantas@gmail.com <n.moraes.dantas@gmail.com>
2017-03-24 01:13:43 -0300

    New user controller methods

--- controllers/user.js
+++ controllers/user.js
@@ -7,11 +7,34 @@
 
 'use strict';
 
-var router  = require('express').Router();
-var business= require('./../business/user');
+var router      = require('express').Router();
+var business    = require('./../business/user');
+var framework   = require('swt-framework');
 
+// Login
 router.post('/login', business.login);
-
+// Logout
 router.post('/logout', business.logout);
+
+// Obtem todos usuarios
+router.get('/', framework.middlewares.authorize('Admin'));
+// Cria um novo usuario
+router.post('/', framework.middlewares.authorize('Admin'));
+// Atualiza todos os usuarios
+router.put('/', framework.middlewares.authorize('Admin'));
+// Apaga/Desativa todos os usuarios
+router.delete('/', framework.middlewares.authorize('Admin'));
+
+// Obtem o usuario especifico
+router.get('/:id');
+// Atualiza o usuario especifico
+router.put('/:id'); 
+// Apaga/Desativa o usuario especifico
+router.delete(':/id');
+
+// Atualiza a senha
+router.put('/:id/password');
+// Reinicia senha
+router.patch('/:id/password');
 
 module.exports = router;
